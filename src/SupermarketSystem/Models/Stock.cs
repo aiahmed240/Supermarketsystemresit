749beb533c1c7a744a78f5703ef815ffb7b6e0c5
aiahmed240 <ai677@live.mdx.ac.uk>
2026-07-04 17:01:32 +0100

    Updated models with properties and relationships

--- src/SupermarketSystem/Models/Stock.cs
+++ src/SupermarketSystem/Models/Stock.cs
@@ -2,7 +2,9 @@ namespace SupermarketSystem.Models
 {
     public class Stock
     {
-        public string ProductBarcode { get; set; } // Which product
-        public int Quantity { get; set; }          // How many in stock
+        public int StockId { get; set; }
+        public int ProductId { get; set; }
+        public int Quantity { get; set; }
+        public DateTime? RestockDate { get; set; }
     }
 }
