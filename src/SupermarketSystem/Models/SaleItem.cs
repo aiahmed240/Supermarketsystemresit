417c38978771977041dc76a74f20113d92d051cd
aiahmed240 <ai677@live.mdx.ac.uk>
2026-07-04 17:03:50 +0100

    Added SaleItem model and linked it to Sale and Product

---
+++ src/SupermarketSystem/Models/SaleItem.cs
@@ -0,0 +1,12 @@
+namespace SupermarketSystem.Models
+{
+    public class SaleItem
+    {
+        public int SaleItemId { get; set; }
+        public int SaleId { get; set; }
+        public int ProductId { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+    }
+}
+
