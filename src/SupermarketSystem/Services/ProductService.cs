36fa816e59c14e479b081748e803dbd8ba50d7d2
aiahmed240 <ai677@live.mdx.ac.uk>
2026-07-04 16:59:57 +0100

    Cleaned product service and fixed product retrieval

--- src/SupermarketSystem/Services/ProductService.cs
+++ src/SupermarketSystem/Services/ProductService.cs
@@ -1,23 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using SupermarketSystem.Data;
 using SupermarketSystem.Models;
 
 namespace SupermarketSystem.Services
 {
     public class ProductService
     {
-        private readonly List<Product> _products = new();
+        private readonly SupermarketDbContext _context;
+
+        public ProductService(SupermarketDbContext context)
+        {
+            _context = context;
+        }
 
         public void AddProduct(Product product)
         {
-            if (_products.Any(p => p.Barcode == product.Barcode))
-                throw new Exception("A product with this barcode already exists.");
-
-            _products.Add(product);
+            _context.Products.Add(product);
+            _context.SaveChanges();
         }
 
         public List<Product> GetAllProducts()
         {
-            return _products;
+            return _context.Products.ToList();
+        }
+
+        public Product? GetProduct(int id)
+        {
+            return _context.Products.FirstOrDefault(p => p.ProductId == id);
+        }
+
+        public void DeleteProduct(int id)
+        {
+            var product = _context.Products.FirstOrDefault(p => p.ProductId == id);
+            if (product != null)
+            {
+                _context.Products.Remove(product);
+                _context.SaveChanges();
+            }
         }
     }
 }
-
