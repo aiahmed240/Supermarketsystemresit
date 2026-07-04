82b667e990b6c185e888bdef222d063c640bc6e3
aiahmed240 <ai677@live.mdx.ac.uk>
2026-07-04 17:02:19 +0100

    Updated models with properties and relationships

--- src/SupermarketSystem/Models/Sale.cs
+++ src/SupermarketSystem/Models/Sale.cs
@@ -2,9 +2,8 @@ namespace SupermarketSystem.Models
 {
     public class Sale
     {
-        public int Id { get; set; }          // Unique sale number
-        public string ProductBarcode { get; set; }  // Which product was sold
-        public int Quantity { get; set; }    // How many items
-        public double TotalPrice { get; set; } // Total price of the sale
+        public int SaleId { get; set; }
+        public DateTime Date { get; set; }
+        public decimal TotalAmount { get; set; }
     }
 }
